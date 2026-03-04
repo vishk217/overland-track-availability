b5e2329728b32de346766d2f290221c5bab4c289
vishal.katwe <vishal.katwe@vocus.com.au>
2026-03-04 23:59:54 +1100

    chore: remove duplicate resource

--- terraform/modules/api-gateway/outputs.tf
+++ terraform/modules/api-gateway/outputs.tf
@@ -2,8 +2,6 @@ output "api_gateway_url" {
   value = "https://${aws_api_gateway_rest_api.overland_api.id}.execute-api.${data.aws_region.current.name}.amazonaws.com/${aws_api_gateway_stage.overland_api_stage.stage_name}"
 }
 
-data "aws_region" "current" {}
-
 output "api_gateway_id" {
   value = aws_api_gateway_rest_api.overland_api.id
 }
