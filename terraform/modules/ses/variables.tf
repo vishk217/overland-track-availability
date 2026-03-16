68f668d5d21172cd4fde141555dae33755b2dc43
vishal.katwe <vishal.katwe@vocus.com.au>
2026-03-16 14:39:08 +1100

    chore: set up records in route 53 for SES

--- terraform/modules/ses/variables.tf
+++ terraform/modules/ses/variables.tf
@@ -2,3 +2,8 @@ variable "domain_name" {
   description = "Domain name for SES identity"
   type        = string
 }
+
+variable "aws_region" {
+  description = "AWS region for SES MAIL FROM MX record"
+  type        = string
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -35,22 +35,6 @@ output "data_bucket_name" {
 }
 
 output "ses_verification_token" {
-  description = "Add this TXT record in Namecheap: Host=_amazonses, Value=this token"
+  description = "SES domain verification token"
   value       = module.ses.verification_token
-}
-
-output "ses_dkim_tokens" {
-  description = "Add these CNAME records in Namecheap for DKIM"
-  value = [for token in module.ses.dkim_tokens : {
-    host  = "${token}._domainkey"
-    value = "${token}.dkim.amazonses.com"
-  }]
-}
-
-output "ses_mail_from_records" {
-  description = "Add these records in Namecheap for MAIL FROM domain"
-  value = {
-    mx_record  = { host = "mail", value = "feedback-smtp.ap-southeast-2.amazonses.com", priority = 10 }
-    spf_record = { host = "mail", type = "TXT", value = "v=spf1 include:amazonses.com ~all" }
-  }
 }
